f1b9117f9c776e3dc637dab5b898ea8080776e5c
huiminlim <e0036379@u.nus.edu>
2022-12-10 23:27:21 +0800

    Add caesar cipher code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,72 @@
+// To be implemented by structs that own
+// the Cipher trait
+trait Cipher {
+    // Takes in a borrowed (takes no ownership of) message string
+    // and returns encrypted message (and the receiver takes ownership)
+    fn encrypt(&self, msg: &str) -> String {
+        // NOTE:
+        // Default unimplemented behaviour
+        // returns original input
+        msg.to_owned()
+    }
+
+    // Takes in a borrowed (takes no ownership of) message string
+    // and returns decrypted message (and the receiver takes ownership)
+    fn decrypt(&self, enc: &str) -> String {
+        // NOTE:
+        // Default unimplemented behaviour
+        // returns original input
+        enc.to_owned()
+    }
+}
+
+struct Caesar {
+    shift: u32,
+}
+impl Caesar {
+    #![allow(dead_code)]
+    fn new(shift: u32) -> Self {
+        Self { shift }
+    }
+
+    fn set_shift(&mut self, _shift: u32) {
+        self.shift = _shift;
+    }
+}
+impl Default for Caesar {
+    fn default() -> Self {
+        Self { shift: 10 }
+    }
+}
+impl Cipher for Caesar {
+    fn encrypt(&self, msg: &str) -> String {
+        // TODO: Fill in logic
+        let uppercase_msg = msg.to_uppercase();
+        let mut chars: Vec<u32> = uppercase_msg.chars().map(|c| c as u32).collect();
+        for char in &mut chars {
+            if *char != (' ' as u32) {
+                *char = (*char - 65 + self.shift) % 26 + 65;
+            }
+        }
+
+        chars
+            .iter()
+            .flat_map(|c| std::char::from_u32(*c))
+            .collect::<String>()
+            .to_owned()
+    }
+
+    fn decrypt(&self, enc: &str) -> String {
+        // TODO: Fill in logic
+        enc.to_owned()
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let mut c = Caesar::default();
+    c.set_shift(4);
+    let encrypted = c.encrypt("ATTACKATONCE");
+    println!("Encrypted message: {:?}", encrypted);
 }
